8494d3ecaa178a1bfa196152ad919fe20c366c97
Tbotny <tbotny@gmail.com>
2019-05-28 19:34:43 +0300

    change port

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const app = express();
 
 app.get('/', (req, res) => res.json({"name": "Evrosale"}));
 
-app.listen(5050, () => console.log('Server ready'));
+app.listen(process.env.PORT || 5050, () => console.log('Server ready'));
