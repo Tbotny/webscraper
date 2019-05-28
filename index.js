0f393df0ef07393239a70aa035e588f529836b16
Tbotny <tbotny@gmail.com>
2019-05-28 22:14:09 +0300

    scraping hm

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
 const express = require('express');
+const puppeteer = require('puppeteer');
+
+
+async function getProducts() {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://www2.hm.com/en_us/sale/kids/view-all.html');
+
+    const result = await page.evaluate(() => {
+
+        const products = Array.from(document.querySelectorAll('.product-item'));
+
+        const result = products.map(item => {
+            const title = item.querySelector('.link').innerText;
+            const price = item.querySelector('.price').innerText;
+
+            return {
+                title,
+                price,
+            }
+
+        });
+
+
+        return {
+            products: result
+        }
+    });
+
+    console.log(result);
+
+    browser.close();
+
+    return result;
+}
 
 const app = express();
 
-app.get('/', (req, res) => res.json({"name": "Evrosale"}));
+app.get('/', async (req, res) => {
+    res.send(await getProducts());
+});
 
 app.listen(process.env.PORT || 5050, () => console.log('Server ready'));
