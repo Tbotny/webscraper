dd7d8a9a1b3d99704c9413640a36f8e4db0ed8b0
Tbotny <tbotny@gmail.com>
2019-05-28 19:13:08 +0300

    change send to json

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-// CommonJS - Node.js
 const express = require('express');
-
-// ES6 Modules - browser, Node.js last version >10
-// import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Evrosale'));
+app.get('/', (req, res) => res.json({"name": "Evrosale"}));
 
 app.listen(5050, () => console.log('Server ready'));
